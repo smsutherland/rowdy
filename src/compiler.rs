129a8f2c4f34f5f6f38a492bacf216f51ff7002a
smsutherland <sagan@farthinghalearms.com>
2021-12-21 20:55:46 -0500

    extract function creation into function

--- src/compiler.rs
+++ src/compiler.rs
@@ -1,66 +1,97 @@
-use crate::token::{self, Token, DataType};
+use crate::token::{self, DataType, Token};
 use std::collections::HashMap;
 
+type FunctionMap = HashMap<FunctionSignature, Vec<Token>>;
 pub fn compile_tokens(tokens: &Vec<Token>) {
-    // let entry = find_entry(&tokens).unwrap();
-    // println!("starting at token {}", entry);
+    let functions = make_functions(tokens);
 
-    let mut functions: HashMap<FunctionSignature, Vec<Token>> = HashMap::new();
+    let main_signature: FunctionSignature = FunctionSignature{
+        return_type: DataType::Int,
+        name: String::from("main"),
+        parameters: Vec::new(),
+    };
+    
+    assert!(functions.contains_key(&main_signature), "program needs main function");
 
+    println!("{:#?}", functions);
+}
+
+fn make_functions(tokens: &Vec<Token>) -> FunctionMap {
+    let mut functions: FunctionMap = HashMap::new();
     let mut i = 0;
-    while i < tokens.len(){
-        if let token::TokenType::TokenFuncDecl(token::FunctionDecl{return_type, name}) = &tokens[i].typ{
+    while i < tokens.len() {
+        if let token::TokenType::TokenFuncDecl(token::FunctionDecl { return_type, name }) =
+            &tokens[i].typ
+        {
             i += 1;
-            debug_assert!(matches!(tokens[i].typ, token::TokenType::TokenSpecialChar(token::SpecialChar::LParen(_))));
-            let mut func_def = FunctionSignature{
+            debug_assert!(matches!(
+                tokens[i].typ,
+                token::TokenType::TokenSpecialChar(token::SpecialChar::LParen(_))
+            ));
+            let mut func_def = FunctionSignature {
                 return_type: *return_type,
                 name: String::from(name),
                 parameters: Vec::new(),
             };
             i += 1;
 
-            while let token::TokenType::TokenVarDecl(token::VarDecl{var_type, name}) = &tokens[i].typ{
+            while let token::TokenType::TokenVarDecl(token::VarDecl { var_type, name }) =
+                &tokens[i].typ
+            {
                 func_def.parameters.push((*var_type, String::from(name)));
                 i += 1;
             }
-            debug_assert!(matches!(tokens[i].typ, token::TokenType::TokenSpecialChar(token::SpecialChar::RParen(_))));
+
+            debug_assert!(matches!(
+                tokens[i].typ,
+                token::TokenType::TokenSpecialChar(token::SpecialChar::RParen(_))
+            ));
+
+            assert!(check_for_duplicate_function(functions.keys(), &func_def));
+
             i += 1;
-            if let token::TokenType::TokenSpecialChar(token::SpecialChar::LBrace(Some(end_index))) = tokens[i].typ{
+            if let token::TokenType::TokenSpecialChar(token::SpecialChar::LBrace(Some(end_index))) =
+                tokens[i].typ
+            {
                 i += 1;
                 functions.insert(func_def, tokens[i..end_index].to_vec());
-                i = end_index+1;
-            }
-            else{
+                i = end_index + 1;
+            } else {
                 unreachable!();
             }
-        }
-        else{
+        } else {
             i += 1;
         }
     }
+    functions
+}
 
-    println!("{:#?}", functions);
+fn check_for_duplicate_function<'a>(
+    existing_signatures: impl Iterator<Item = &'a FunctionSignature>,
+    new_signature: &FunctionSignature,
+) -> bool {
+    for signature in existing_signatures {
+        if !new_signature.is_compatible(&signature) {
+            return false;
+        }
+    }
+    true
 }
 
 #[derive(PartialEq, Eq, Hash, Debug)]
-struct FunctionSignature{
+struct FunctionSignature {
     return_type: DataType,
     name: String,
     parameters: Vec<(DataType, String)>,
 }
 
-// fn find_entry(tokens: &Vec<Token>) -> Option<usize>{
-//     let mut entry_index = None;
-//     for (i, token) in tokens.iter().enumerate() {
-//         if token.is_entry(){
-//             if entry_index == None{
-//                 entry_index = Some(i);
-//             }
-//             else{
-//                 panic!("multiple entry points");
-//             }
-//         }
-//     }
-//     entry_index
-// }
-// 0: Token { typ: TokenFuncDecl(FunctionDecl { return_type: Int, name: "main" }), loc: .\test.ry:1:1 }
+impl FunctionSignature {
+    fn is_compatible(&self, other: &Self) -> bool {
+        self.name != other.name
+            || !self
+                .parameters
+                .iter()
+                .map(|x| x.0)
+                .eq(other.parameters.iter().map(|x| x.0))
+    }
+}
