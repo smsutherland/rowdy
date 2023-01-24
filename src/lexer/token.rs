ce2884b3f587093aaf96a599f379fb8425a5f1c8
Sagan Sutherland <sagan@farthinghalearms.com>
2023-01-24 01:55:39 -0500

    qualify kewords

--- src/lexer/token.rs
+++ src/lexer/token.rs
@@ -84,10 +84,35 @@ pub struct QualifiedToken {
 
 pub fn qualify_token(token: Token, code: &str) -> QualifiedToken {
     match token.typ {
-        TokenType::Symbol => QualifiedToken {
-            typ: QualifiedTokenType::Symbol(token.span.slice(code).to_string()),
-            span: token.span,
-        },
+        TokenType::Symbol => {
+            let symbol = token.span.slice(code);
+            match symbol {
+                "if" => QualifiedToken {
+                    typ: QualifiedTokenType::Keyword(Keyword::If),
+                    span: token.span,
+                },
+                "for" => QualifiedToken {
+                    typ: QualifiedTokenType::Keyword(Keyword::For),
+                    span: token.span,
+                },
+                "else" => QualifiedToken {
+                    typ: QualifiedTokenType::Keyword(Keyword::Else),
+                    span: token.span,
+                },
+                "while" => QualifiedToken {
+                    typ: QualifiedTokenType::Keyword(Keyword::While),
+                    span: token.span,
+                },
+                "return" => QualifiedToken {
+                    typ: QualifiedTokenType::Keyword(Keyword::Return),
+                    span: token.span,
+                },
+                _ => QualifiedToken {
+                    typ: QualifiedTokenType::Symbol(symbol.to_string()),
+                    span: token.span,
+                },
+            }
+        }
         TokenType::Operator(op) => QualifiedToken {
             typ: QualifiedTokenType::Operator(op),
             span: token.span,
